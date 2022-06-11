4a62520507e47118601810c70e1b6b0bf2e542f0
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2022-06-11 17:00:30 -0400

    add reducer function and rendering to app.js

--- src/App.js
+++ src/App.js
@@ -2,16 +2,16 @@ import { useReducer } from 'react';
 import './styles.css';
 
 function reducer(state, action) {
-  
+
 }
 
 function App() {
-  const [state, dispatch] = useReducer(reducer, {});
+  const [{currentOperand, previousOperand, operation }, dispatch] = useReducer(reducer, {});
   return (
     <div className="calculator-grid">
       <div className="output">
-        <div className="previous-operand">123,234 *</div>
-        <div className="current-operand">123,234</div>
+        <div className="previous-operand">{previousOperand} {operation}</div>
+        <div className="current-operand">{currentOperand}</div>
       </div>
       <button className="span-two">AC</button>
       <button>DEL</button>
